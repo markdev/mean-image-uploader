3e40fdb7f59dcc9afa030ef026ec18b09fed5f0e
Mark Karavan <mark.karavan@gmail.com>
2014-11-08 02:50:32 -0500

    more messing around

--- public/app/test.js
+++ public/app/test.js
@@ -1,13 +1,43 @@
 // declare a module
-angular.module('webapp', [])
+angular
+	.module('app', [
+		'ui.router'
+	])
+	.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'templates/home.html'
+			})
+			.state('about', {
+				url: '/about',
+				templateUrl: 'templates/about.html'
+			})
+			.state('contact', {
+				url: '/contact',
+				templateUrl: 'templates/contact.html'
+			})
+	}])
 
 
-.controller('MainController', function($scope) {
-	$scope.val = 1;
-	$scope.even = false;
 
-	$scope.inc = function() {
-		$scope.val += 1;
-		$scope.even = ($scope.val % 2 == 0)? true : false;
-	}
-})
+
+/*
+	.controller('MainController', function($scope) {
+		$scope.val = 1;
+		$scope.even = false;
+		$scope.foo.val = "this is it";
+
+		$scope.inc = function() {
+			$scope.val += 1;
+			$scope.even = ($scope.val % 2 == 0)? true : false;
+		}
+	})
+
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {templateUrl: 'view.html', controller: 'MainController'});
+	})
+*/
