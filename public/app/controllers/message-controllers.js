3665f2c1ebbb8f7fe60aea8dd8f4c51a92630ddf
Mark Karavan <mark.karavan@gmail.com>
2014-11-10 23:38:20 -0500

    adds controllers

---
+++ public/app/controllers/message-controllers.js
@@ -0,0 +1,1 @@
+console.log("loaded: message controllers");
---
+++ public/app/controllers/user-controllers.js
@@ -0,0 +1,62 @@
+console.log("loaded: user controllers");
+
+angular
+	.module('yote')
+
+	.controller('SunzoraCtrl', ['$scope', '$stateParams', '$state', '$location', 'SunzoraFactory', function($scope, $stateParams, $state, $location, SunzoraFactory){
+		console.log('SunzoraCtrl loaded...');
+	}])
+
+	.controller('UserLogoutCtrl', ['$scope', '$stateParams', '$state', '$location', 'SunzoraFactory', 'UserFactory', function($scope, $stateParams, $state, $location, SunzoraFactory, UserFactory){
+		$scope.logout = function() {
+			console.log("logging out");
+			UserFactory.logout()
+				.then(function(data) {
+					console.log(data);
+					console.log("now redirect");
+					$state.go('login')
+				})
+
+		}
+	}])
+
+	.controller('UserSignupCtrl', ['$scope', '$stateParams', '$state', '$location', 'SunzoraFactory', 'UserFactory', function($scope, $stateParams, $state, $location, SunzoraFactory, UserFactory){
+		$scope.email = 'mark.karavan@gmail.com';
+		$scope.password = 'mark';
+		$scope.password2 = 'mark';
+		$scope.signup = function() {
+			var postData = {};
+			postData.email = $scope.email;
+			postData.password = $scope.password;
+			postData.password2 = $scope.password2;
+			console.log(postData);
+			UserFactory.signup(postData)
+				.then(function(data) {
+					console.log(data)
+				})
+
+		}
+	}])
+
+	.controller('UserLoginCtrl', ['$scope', '$stateParams', '$state', '$rootScope', 'SunzoraFactory', 'UserFactory', function($scope, $stateParams, $state, $rootScope, SunzoraFactory, UserFactory) {
+		$scope.email = "mark.karavan@gmail.com";
+		$scope.password = "mark";
+		$scope.loginAction = function() {
+			var postData = {};
+			postData.email = $scope.email;
+			postData.password = $scope.password;
+			UserFactory.login(postData)
+				.then(function(data) {
+					console.log(data);
+					if (data.success) {
+						$rootScope.currentUser = data.user;
+						$state.go('root')
+					}
+				})
+		};
+	/*
+		$scope.logout = function() {
+			UserFactory.logout();
+		};
+	*/
+	}])
