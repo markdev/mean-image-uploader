ebc8f1f44a5310a040de561690d3f5910394d1e4
Mark Karavan <mark.karavan@gmail.com>
2014-11-15 04:10:27 -0500

    contests are a shit-show but they work

--- public/app/controllers/contest-controllers.js
+++ public/app/controllers/contest-controllers.js
@@ -9,10 +9,29 @@ angular
 		$scope.tags = "Felines, Cats, Kitties";
 		$scope.rules = "Must be a cute kitty";
 		$scope.deadline = new Date();
+		$scope.months = [ {name: "Jan"}, {name: "Feb"}, {name: "Mar"}, {name: "Apr"}, {name: "May"}, {name: "Jun"}, {name: "Jul"}, {name: "Aug"}, {name: "Sep"}, {name: "Oct"}, {name: "Nov"}, {name: "Dec"} ];
+		$scope.myMonth = $scope.months[0];
+		$scope.days = [ {name: "1"}, {name: "2"}, {name: "3"}, {name: "4"}, {name: "5"}, {name: "6"}, {name: "7"}, {name: "8"}, {name: "9"}, {name: "10"}, {name: "11"}, {name: "12"}, {name: "13"}, {name: "14"}, {name: "15"}, {name: "16"}, {name: "17"}, {name: "18"}, {name: "19"}, {name: "20"}, {name: "21"}, {name: "22"}, {name: "23"}, {name: "24"}, {name: "25"}, {name: "26"}, {name: "27"}, {name: "28"}, {name: "29"}, {name: "30"}, {name: "31"} ];
+		$scope.myDay = $scope.days[0];
+		$scope.years = [{name: "2014"}, {name: "2015"}, {name: "2016"}];
+		$scope.myYear = $scope.years[0];
+		$scope.hours = [ {name: "00"}, {name: "01"}, {name: "02"}, {name: "03"}, {name: "04"}, {name: "05"}, {name: "06"}, {name: "07"}, {name: "08"}, {name: "09"}, {name: "10"}, {name: "11"}, {name: "12"}, {name: "13"}, {name: "14"}, {name: "15"}, {name: "16"}, {name: "17"}, {name: "18"}, {name: "19"}, {name: "20"}, {name: "21"}, {name: "22"}, {name: "23"} ];
+		$scope.myHour = $scope.hours[0];
+		$scope.minutes = [ {name: "00"}, {name: "05"}, {name: "10"}, {name: "15"}, {name: "20"}, {name: "25"}, {name: "30"}, {name: "35"}, {name: "40"}, {name: "45"},{name: "50"},{name: "55"} ];
+		$scope.myMinute = $scope.minutes[0];
 		$scope.create = function() {
+			//Sat Nov 15 2014 03:57:34 GMT-0500 (EST)
+			var deadline = 'Sat ';
+			deadline += $scope.myMonth.name + ' ';
+			deadline += $scope.myDay.name + ' ';
+			deadline += $scope.myYear.name + ' ';
+			deadline += $scope.myHour.name + ':';
+			deadline += $scope.myMinute.name + ':00 GMT-0500 (EST)';
+			//console.log(deadline);
+			
 			var postData = {};
 			postData.title = $scope.title;
-			postData.deadline = $scope.deadline;
+			postData.deadline = deadline;
 			postData.tags = [];
 			$.each($scope.tags.split(','), function() {
     			postData.tags.push($.trim(this).toLowerCase());
