56c4b3a8c7884453bf71536364ee1872d3ddb236
Mark Karavan <mark.karavan@gmail.com>
2014-11-19 00:04:55 -0500

    some kinda uploader

--- public/app/controllers/contest-controllers.js
+++ public/app/controllers/contest-controllers.js
@@ -229,3 +229,18 @@ angular
   			});
 		};
 	}])
+
+
+var UploadController = function ($scope, fileReader) {
+	console.log(fileReader)
+    $scope.getFile = function () {
+    	$scope.progress = 0;
+    	fileReader.readAsDataUrl($scope.file, $scope)
+    		.then(function(result) {
+    			$scope.imageSrc = result;
+    		});
+    	};
+    $scope.$on("fileProgress", function(e, progress) {
+    	$scope.progress = progress.loaded / progress.total;
+    });
+};
--- public/app/directives/sunzora-directives.js
+++ public/app/directives/sunzora-directives.js
@@ -1,1 +1,18 @@
 console.log("loaded: sunzora directives");
+
+'use strict'
+
+angular
+	.module('yote')
+
+	.directive("ngFileSelect", function() {    
+		return {
+			link: function($scope,el) {
+				el.bind("change", function(e) {
+					$scope.file = (e.srcElement || e.target).files[0];
+					$scope.getFile();
+				});
+			}
+		}
+	})
+	;
