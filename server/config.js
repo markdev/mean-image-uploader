557cd43fe13346230106e2077a7aa98f923bfda9
Mark Karavan <mark.karavan@gmail.com>
2014-11-30 13:58:48 -0500

    cleanup

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var rootPath = path.normalize(__dirname + '/../../');
-var os = require('os');
+var path 		= require('path')
+  , rootPath 	= path.normalize(__dirname + '/../../')
+  , os 			= require('os')
+  ;
 
 if (os.hostname() == 'Marks-MacBook-Pro-11.local') {
 	module.exports = {
@@ -23,25 +24,3 @@ if (os.hostname() == 'Marks-MacBook-Pro-11.local') {
     	port: process.env.PORT || 2345
 	}	
 }
-/*
-module.exports = {
-	'Marks-MacBook-Pro-11.local': {
-		db: 'mongodb://127.0.0.1/sunzora',
-		redis: {
-			host: '127.0.0.1',
-			port: '6379'
-		},
-		rootDir: '/Users/markkaravan/sunzora/sunzora0.6/sunzora/',
-		port: process.env.PORT || 2345
-	},
-	'li60-94': {
-		db: 'mongodb://127.0.0.1/sunzora',
-		redis: {
-			host: '127.0.0.1',
-			port: '6379'
-		},
-		rootDir: '/var/www/sunzora-proto/',
-    	port: process.env.PORT || 2345
-	}
-}
-*/
